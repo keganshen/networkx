94f65810a74fd48b73f025dceb3fc825d3719222
Kegan <kegan_shen@163.com>
2016-07-08 17:18:25 +0800

    误删改回

--- test6.py
+++ test6.py
@@ -2,7 +2,7 @@
 import csv
 import networkx as nx
 import matplotlib.pyplot as plt
-
+'''
 g=nx.Graph()
 g.add_edge(u'张三',u'李四')
 g.add_edge(u'张三',u'王五')
@@ -18,14 +18,13 @@ for row in reader:
                 continue
         for i in range(len(row)):
                 print row[i]
-        G.add_edge(row[0],row[1])
+        G.add_edge(row[0].decode('utf-8'),row[1].decode('utf-8'))
         #print len(row)
         #new_item = tuple(row)
         #aaa= '---'.join(new_item).decode('utf-8')
         #G.add_edge(*new_item)
 
 #print nx.degree(G)
-pos = nx.random_layout(G)
+pos = nx.spring_layout(G)
 nx.draw_networkx(G,pos)
 plt.show()
-'''
